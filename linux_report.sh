a075a740d258daf2a36b479fc05516ace8d68ce6
kinghow <kinghow@hotmail.com>
2021-04-26 15:33:17 +0800

    update

--- linux_report.sh
+++ linux_report.sh
@@ -2,4 +2,4 @@
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$PATH"
 report_base=/var/log/linux_report
 datetime=`date +%Y-%m-%d_%H-%M`
-python3 $report_base/linux_report.py -p $report_base/linux_report.ini  -s txt>$report_base/activeio_report_$datetime.log
+python3 $report_base/linux_report.py -p $report_base/linux_report.ini  -s txt>$report_base/linux_report_$datetime.log
