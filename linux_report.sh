4a0807822309bcf7387fae521fee5f521a4c1420
kinghow <kinghow@hotmail.com>
2021-04-28 16:09:00 +0800

    update

--- linux_report.sh
+++ linux_report.sh
@@ -4,3 +4,4 @@ report_base=/var/log/linux_report
 datetime=`date +%Y-%m-%d_%H-%M`
 python3 $report_base/linux_report.py -p $report_base/linux_report.ini  -s txt>$report_base/linux_report_$datetime.log
 python $report_base/SendEmail.py -p $report_base/emailset.ini -f $report_base/linux_report_$datetime.log
+find $report_base -mtime +30 -name "linux_report_*.log" -exec rm -rf {} \;
